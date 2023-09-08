15144267c8087d6cda7bd8ffedbf023752b134b8
pvillor <pvillor@gmail.com>
2023-09-08 13:36:06 -0300

    first commit

---
+++ src/main/java/br/com/villo/spring/Application.java
@@ -0,0 +1,23 @@
+package br.com.villo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/hello-world")
+	public ResponseEntity<String> readHelloWorld() {
+		return new ResponseEntity<String>("Hello World!", HttpStatus.OK);
+	}
+
+}
